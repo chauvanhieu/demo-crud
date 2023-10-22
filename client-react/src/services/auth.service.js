46d76c5852eb8cc068beabf60534649313368773
chauvanhieu <chauhieu.nina@gmail.com>
2023-10-23 04:40:33 +0700

    login logic

--- client-react/src/services/auth.service.js
+++ client-react/src/services/auth.service.js
@@ -1,4 +1,13 @@
-/** @format */
+import AxiosService from "./axios.service";
 
-const AuthService = {};
+const AuthService = {
+  login: async (username, password) => {
+    const data = await AxiosService.post({
+      url: "login",
+      data: { username, password },
+    });
+
+    return data;
+  },
+};
 export default AuthService;
